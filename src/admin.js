9c90da3837be73b59c04c238709c6ea46f8c8cfa
ningliu <ningliu@sf-express.com>
2018-11-13 19:46:55 +0800

    搭建页面结构

---
+++ src/admin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NavLeft from './components/NavLeft';
+
+
+export default class Admin extends React.Component {
+  render() {
+    return (
+      <Row>
+        <Col span={3}>
+          <NavLeft />
+        </Col>
+        <Col span={21}>
+          <Header />
+          <Row>
+            Content
+          </Row>
+          <Footer />
+        </Col>
+      </Row>
+    );
+  }
+}
