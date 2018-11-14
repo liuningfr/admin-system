6bf5850d55558d51fab67861ee6947a72e0317ba
ningliu <ningliu@sf-express.com>
2018-11-14 15:06:50 +0800

    开发菜单

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Menu } from 'antd';
+import menuConfig from './../../config/menuConfig';
+import './index.less';
 
-export default class NavLeft extends React.Component {
+const SubMenu = Menu.SubMenu;
+class NavLeft extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuTreeNode: null,
+    };
+  }
+  componentWillMount() {
+    const menuTreeNode =  this.renderMenu(menuConfig);
+    console.log(menuTreeNode);
+    this.setState({ menuTreeNode });
+  }
+
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if(item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)} 
+          </SubMenu>
+        );
+      }
+      return (
+        <Menu.Item title={item.title} key={item.key}>
+          {item.title}
+        </Menu.Item>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        This is NavLeft
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt="" />
+          <h1>My Ant Design</h1>
+        </div>
+        <Menu
+          theme="dark" 
+        >
+          {this.state.menuTreeNode}
+        </Menu>
       </div>
     );
   }
 }
+
+export default NavLeft;
