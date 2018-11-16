ac1a4acdf4424245e16532a6d0fdfdfc7950575a
ningliu <ningliu@sf-express.com>
2018-11-16 15:00:59 +0800

    按钮部分完成

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Card, Button, Icon } from 'antd';
+import { Card, Button, Radio } from 'antd';
 import './buttons.less';
 class Buttons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      size: 'default',
+    };
+  }
+  handleClose = () => {
+    this.setState({ loading: false });
+  }
+  handleLoad = () => {
+    this.setState({ loading: true });
+  }
+  handleChange = e => {
+    this.setState({ size: e.target.value });
+  }
   render() {
+    const { loading, size } = this.state;
     return (
       <div>
         <Card title="基础按钮">
@@ -21,11 +38,29 @@ class Buttons extends React.Component {
           <Button type="primary" icon="download">下载</Button>
         </Card>
         <Card title="Loading按钮">
-          <Button type="primary" loading>确定</Button>
-          <Button type="primary" shape="circle" loading />
-          <Button loading>点击加载</Button>
-          <Button shape="circle" loading />
-          <Button type="primary">关闭</Button>
+          <Button type="primary" loading={loading}>确定</Button>
+          <Button type="primary" shape="circle" loading={loading} />
+          <Button loading={loading} onClick={this.handleLoad}>点击加载</Button>
+          <Button shape="circle" loading={loading} />
+          <Button type="primary" onClick={this.handleClose}>关闭</Button>
+        </Card>
+        <Card title="按钮组">
+          <Button.Group>
+            <Button type="primary" icon="left">返回</Button>
+            <Button type="primary" icon="right">前进</Button>
+          </Button.Group>
+        </Card>
+        <Card title="按钮尺寸">
+          <Radio.Group value={size} onChange={this.handleChange}>
+            <Radio value="small">小</Radio>
+            <Radio value="default">中</Radio>
+            <Radio value="large">大</Radio>
+          </Radio.Group>
+          <Button type="primary" size={size}>Primary</Button>
+          <Button size={size}>Default</Button>
+          <Button type="dashed" size={size}>Dashed</Button>
+          <Button type="danger" size={size}>Danger</Button>
+          <Button disabled size={size}>Disabled</Button>
         </Card>
       </div>
     );
