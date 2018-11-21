aff4402292ce487f91b6f7b265f6a808864f0ede
ningliu <ningliu@sf-express.com>
2018-11-21 12:11:20 +0800

    update table

--- src/axios/index.js
+++ src/axios/index.js
@@ -20,16 +20,20 @@ export default class Axios {
     });
   }
   static ajax(options) {
+    const loading = document.getElementById('ajaxLoading');
+    loading.style.display = 'block';
     const baseURL =
       'https://www.easy-mock.com/mock/5bf3fb5258243e76789c204c/mockapi/';
+    const { url, method, params } = options;
     return new Promise((resolve, reject) => {
       axios({
-        url: options.url,
-        method: 'get',
+        url,
+        method,
+        params: params || '',
         baseURL,
-        timeout: 5000,
-        params: options.data || ''
+        timeout: 5000
       }).then(response => {
+        loading.style.display = 'none';
         if (response.status === 200) {
           const res = response.data;
           if (res.code === 0) {
--- src/pages/table/basic.js
+++ src/pages/table/basic.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Card, Table } from 'antd';
 import axios from './../../axios';
 
+const states = {
+  1: '状态一',
+  2: '状态二'
+};
+const hobbies = {
+  1: '音乐',
+  2: '运动'
+};
 const columns = [
   {
     title: 'id',
@@ -13,15 +21,20 @@ const columns = [
   },
   {
     title: '性别',
-    dataIndex: 'sex'
+    dataIndex: 'sex',
+    render: text => (text === 1 ? '男' : '女')
   },
   {
     title: '状态',
-    dataIndex: 'state'
+    dataIndex: 'state',
+    render: text => states[text]
   },
   {
     title: '爱好',
-    dataIndex: 'hobby'
+    dataIndex: 'hobby',
+    render: text => {
+      return text.map(item => hobbies[item]).join(', ');
+    }
   },
   {
     title: '生日',
@@ -42,7 +55,8 @@ class BasicTable extends React.Component {
     axios
       .ajax({
         url: '/table/list',
-        data: {
+        method: 'get',
+        params: {
           page: 1
         }
       })
