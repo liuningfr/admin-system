0464804e3db4389fe65166564021827b449ef2d9
ningliu <ningliu@sf-express.com>
2018-11-27 18:31:40 +0800

    update city

--- src/pages/city/index.js
+++ src/pages/city/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from './../../axios';
-import { Card, Form, Select, Button, Table } from 'antd';
+import { Card, Form, Select, Button, Table, Modal } from 'antd';
 import './index.less';
 
 const columns = [
@@ -24,7 +24,8 @@ class City extends React.Component {
     super(props);
     this.state = {
       list: [],
-      page: 1
+      page: 1,
+      showOpenCity: false
     };
   }
   componentDidMount() {
@@ -40,6 +41,18 @@ class City extends React.Component {
         this.setState({ list: res.data });
       });
   };
+  handleOpenCity = () => {
+    this.setState({ showOpenCity: true });
+  };
+  handleSubmit = () => {
+    this.openCityForm.props.form.validateFields((errors, values) => {
+      console.log(values);
+    });
+  };
+  handleCancel = () => {
+    this.openCityForm.props.form.resetFields();
+    this.setState({ showOpenCity: false });
+  };
   render() {
     return (
       <div>
@@ -47,7 +60,9 @@ class City extends React.Component {
           <FormFiter />
         </Card>
         <Card>
-          <Button type="primary">创建</Button>
+          <Button type="primary" onClick={this.handleOpenCity}>
+            创建
+          </Button>
           <Table
             columns={columns}
             dataSource={this.state.list}
@@ -61,6 +76,18 @@ class City extends React.Component {
             }}
           />
         </Card>
+        <Modal
+          title="创建城市"
+          visible={this.state.showOpenCity}
+          onCancel={this.handleCancel}
+          onOk={this.handleSubmit}
+        >
+          <OpenCityForm
+            wrappedComponentRef={form => {
+              this.openCityForm = form;
+            }}
+          />
+        </Modal>
       </div>
     );
   }
@@ -69,6 +96,12 @@ class City extends React.Component {
 export default City;
 
 class Filter extends React.Component {
+  handleSearch = e => {
+    e.preventDefault();
+    this.props.form.validateFields((errors, values) => {
+      console.log(values);
+    });
+  };
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
@@ -92,7 +125,9 @@ class Filter extends React.Component {
           )}
         </Form.Item>
         <Form.Item style={{ marginRight: 2 }}>
-          <Button type="primary">查询</Button>
+          <Button type="primary" onClick={this.handleSearch}>
+            查询
+          </Button>
         </Form.Item>
         <Form.Item>
           <Button>重置</Button>
@@ -102,3 +137,40 @@ class Filter extends React.Component {
   }
 }
 const FormFiter = Form.create()(Filter);
+
+class OpenCity extends React.Component {
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        span: 5
+      },
+      wrapperCol: {
+        span: 19
+      }
+    };
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form>
+        <Form.Item label="选择城市" {...formItemLayout}>
+          {getFieldDecorator('city_id')(
+            <Select style={{ width: 180 }}>
+              <Select.Option value={0}>全部</Select.Option>
+              <Select.Option value={1}>北京</Select.Option>
+              <Select.Option value={2}>上海</Select.Option>
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item label="选择模式" {...formItemLayout}>
+          {getFieldDecorator('op_mode')(
+            <Select style={{ width: 180 }}>
+              <Select.Option value={0}>全部</Select.Option>
+              <Select.Option value={1}>自营</Select.Option>
+              <Select.Option value={2}>加盟</Select.Option>
+            </Select>
+          )}
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+const OpenCityForm = Form.create()(OpenCity);
